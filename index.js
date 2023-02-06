2e176c2ee1f1775850aaa12dfe12e9f2e3436d57
Orozobekov Dosbol <orozobekovdosbol@gmail.com>
2023-02-06 15:29:53 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,78 @@
+// function rastangle(a,b) {
+//   let s = a * b;
+//   console.log("area is " + s)
+// }
+
+// rastangle(3,5)
+
+// function getvolume(a, b, c) {
+//   let v = a * b * c;
+//   console.log("volume composes " + v)
+// }
+
+// getvolume(4,4,8)
+
+// // function lenght(str) {
+// //   return str.lenght;
+// // }
+
+// function getCircle(circle) {
+//   return 3.14 * (circle**2);
+// }
+
+// console.log(getCircle(6));
+
+// let i = 1;
+// while (i <= 10) {
+//   console.log(i);
+//   i++;
+// }
+
+// // let c = 1;
+// // while (c <= 10) {
+// //   if 
+// // }
+
+// for (let i = 1; i < 10; i += 1) {
+//   if (i % 2 !== 0) {
+//     console.log(i);
+//   }
+// }
+
+// let total = 0;
+// for (let i = 1; i <= 10; i++) {
+//   total = total + i;
+// }
+
+// console.log(total);
+
+
+// for (let i = 1; i < 20; i++) {
+//   if (i % 3 == 0) {
+//     console.log("hello");
+//   }
+//   console.log(i)
+// }
+
+// for (let i = 1; i < 45; i++) {
+//   if (i % 5 == 0 && i % 3 == 0) {
+//     console.log("Fizz buz");
+//   }
+//   else if (i % 5 == 0) {
+//     console.log("Buzz");
+//   }
+//   else if (i % 3 == 0) {
+//     console.log("Fizz");
+//   }
+//   else {
+//     console.log(i)
+//   }
+// }
+
+for (let i = 1; i <= 50; i++) {
+  if (i % 13 === 0) {
+    continue;
+  }
+  console.log(i)
+}
+
